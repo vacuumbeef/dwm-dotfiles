29c3305b7e686ebeeb5504f12d1fd5b99ed04182
streescult <streescult@disroot.org>
2023-06-17 16:29:15 +0300

    bar scripts update

--- scripts/dwm/bar/tags-calc.py
+++ scripts/dwm/bar/tags-calc.py
@@ -2,16 +2,43 @@
 import sys, re
 scan = None
 ocpd = None
- 
+
 def extract(str):
 	return int(re.findall(r'\d+', str)[0])
- 
+
+'''
 def addf(arr, num):
 	if num in arr:
 		return " ".join(str(x) for x in arr).replace(str(num), 'F' + str(num))
 	else:
 		return " ".join(str(x) for x in sorted(arr + [num])).replace(str(num), 'F' + str(num))
- 
+'''
+
+def stringify(arr):
+    return " ".join(str(x) for x in arr)
+
+def replace_element(lst, old_value, new_value):   # thx ChatGPT
+    try:
+        index = lst.index(old_value)
+        lst[index] = new_value
+        return lst
+    except ValueError:
+        print(f"{old_value} not found in the list.")
+
+def addf (arr, num):
+    if num in arr:
+        return replace_element(arr, num, f"F{num}")
+    else:
+        return replace_element(sorted(arr + [num]), num, f"F{num}")
+
+'''
+def dumbhack(string):
+    if "F1F1" in string:
+        return string.replace("F1F1", "F11")
+    else:
+        return string
+'''
+
 def tags(num, power = 16, arr = []):
   if power >= 0:
     if num >= pow(2, power):
@@ -19,13 +46,13 @@ def tags(num, power = 16, arr = []):
     else:
       return tags(num, power - 1, arr)
   return arr[::-1]
- 
+
 for line in sys.stdin:
 	if 'old_state' in line:
 		scan = False
 	if 'new_state' in line:
 		scan = True
 	if 'occupied' in line and scan:
-		print(addf(tags(extract(line)), ocpd))
+		print(stringify(addf(tags(extract(line)), ocpd)))
 	if 'selected' in line and scan:
 		ocpd = tags(extract(line))[0]
